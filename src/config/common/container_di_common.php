42e5d35b3259215c198f68e1ed0f2d674ee98a27
Aleksandr Chelnokov <as@alltel24.ru>
2022-09-30 00:11:41 +0500

    change DB_SCHEMA

--- src/config/common/container_di_common.php
+++ src/config/common/container_di_common.php
@@ -45,7 +45,7 @@ return [
             ['class' => \yii\db\Connection::class],
             [
                 [
-                    'dsn' => env('DB_SHEME') . ':host=' . env('DB_HOST') . ';dbname=' . env('DB_NAME') . ';port=' . env('DB_PORT'),
+                    'dsn' => env('DB_SCHEMA') . ':host=' . env('DB_HOST') . ';dbname=' . env('DB_NAME') . ';port=' . env('DB_PORT'),
                     'username' => env('DB_USER'),
                     'password' => env('DB_PASS'),
                     'charset' => 'utf8',
